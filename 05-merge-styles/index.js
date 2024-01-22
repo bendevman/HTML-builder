35dea333dd91724e80f40deedf560163aa6cdb32
bendevman <bendevman@gmail.com>
2024-01-22 14:25:25 +0300

    feat: add 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir, unlink } = require('fs/promises');
+const { pipeline } = require('stream/promises');
+
+async function createBundle(inputFolder, outputFolder) {
+  const inputPath = path.join(__dirname, inputFolder);
+  const outputPath = path.join(__dirname, outputFolder);
+  const files = await readdir(inputPath, { withFileTypes: true });
+  const fileList = files.filter(
+    (file) => file.isFile() && path.extname(file.name) === '.css',
+  );
+  deleteBundle(outputPath);
+
+  for (const file of fileList) {
+    const readStream = fs.createReadStream(
+      path.join(inputPath, file.name),
+      'utf-8',
+    );
+    const writeStream = fs.createWriteStream(
+      path.join(outputPath, 'bundle.css'),
+      { flags: 'a' },
+    );
+    await pipeline(readStream, writeStream);
+  }
+}
+async function deleteBundle(dirPath) {
+  const files = await readdir(dirPath);
+  for (const file of files) {
+    if (file.name === 'bundle.css') {
+      const filePath = path.join(dirPath, file);
+      await unlink(filePath);
+    }
+  }
+}
+createBundle('styles', 'project-dist');
+// createBundle('test-files/styles', 'test-files');
